fc4430519986fefaad6763a88b399d1ef1ada20b
ade-re10 <andreandre_1234@hotmail.com>
2022-11-19 21:13:18 +0100

    Create a 2D Array

---
+++ src/main/java/com/andresantos/WorkingWith2DArrays.java
@@ -0,0 +1,11 @@
+package com.andresantos;
+
+import java.util.Arrays;
+
+public class WorkingWith2DArrays {
+    public static void main (String[] args) {
+        // Create a 2D Array
+        char [][] board = new char[3][3];
+
+    }
+}
--- src/main/java/com/andresantos/WorkingWithArrays.java
+++ src/main/java/com/andresantos/WorkingWithArrays.java
@@ -1,29 +1,28 @@
 package com.andresantos;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class TheArrays {
+public class WorkingWithArrays {
     public static void main(String[] args) {
         // Create a new Array of string with 5 elements
         String[] colors = new String[5];
         // color in position 0 recives "purple" and color in position 1 recieves "blue"
         colors[0] = "purple";
         colors[1] = "blue";
-        // Print all the array has a string
+        // Print all the array as a string
         System.out.println(Arrays.toString(colors));
         // print color 0 and 1
         System.out.println(colors[0]);
         System.out.println(colors[1]);
         // color in the position 2 is yellow
         colors[2] = "yellow";
-        // Pring the upfated array of colors
+        // Print the updated array of colors
         System.out.println(Arrays.toString(colors));
         // ARray of integers with a fixed size
         int[] numbers = {100, 200};
 
         // Loop through Arrays
-        // 1st - from the firt to the last
+        // 1st - from the first to the last
         for (int i = 0; i < colors.length; i++) {
             System.out.println(colors[i]);
         }
