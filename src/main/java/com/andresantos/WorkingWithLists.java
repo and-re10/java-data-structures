f7217eef9c2284aad964ad2ceaf1bb29088ae03f
ade-re10 <andreandre_1234@hotmail.com>
2022-11-19 21:42:52 +0100

    Create an array list

---
+++ src/main/java/com/andresantos/WorkingWithLists.java
@@ -0,0 +1,12 @@
+package com.andresantos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkingWithLists {
+    public static void main (String [] args) {
+        // Create an Array List
+        List<String> colors = new ArrayList<>();
+
+    }
+}
