373888d8c7cf48376d81aea7a4135fb953c88885
ade-re10 <andreandre_1234@hotmail.com>
2022-11-19 21:13:51 +0100

    Fill the array with '-'

--- src/main/java/com/andresantos/WorkingWith2DArrays.java
+++ src/main/java/com/andresantos/WorkingWith2DArrays.java
@@ -6,6 +6,12 @@ public class WorkingWith2DArrays {
     public static void main (String[] args) {
         // Create a 2D Array
         char [][] board = new char[3][3];
+        // Fill thr array with '-'
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
 
     }
 }
