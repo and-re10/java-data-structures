9eea29dfe4eefe9a30667edbb2442565989e1447
ade-re10 <andreandre_1234@hotmail.com>
2022-11-19 21:14:50 +0100

    Fill the array to create the first line of 'O'µ

--- src/main/java/com/andresantos/WorkingWith2DArrays.java
+++ src/main/java/com/andresantos/WorkingWith2DArrays.java
@@ -12,6 +12,12 @@ public class WorkingWith2DArrays {
                 board[i][j] = '-';
             }
         }
+        // fill the board on column 0 row 0, column 0 row 1 and column 0 row 2 witho 'O'
+        board[0][0] = 'O';
+        board[1][0] = 'O';
+        board[2][0] = 'O';
+        // Print the 2D array as a string
+        System.out.println(Arrays.deepToString(board));
 
     }
 }
