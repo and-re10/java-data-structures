78b836260a307ad8cb8a1f959142810a03ed0da4
ade-re10 <andreandre_1234@hotmail.com>
2022-11-19 21:46:37 +0100

    Add 3 colors and print the list

--- src/main/java/com/andresantos/WorkingWithLists.java
+++ src/main/java/com/andresantos/WorkingWithLists.java
@@ -7,6 +7,15 @@ public class WorkingWithLists {
     public static void main (String [] args) {
         // Create an Array List
         List<String> colors = new ArrayList<>();
+        // Add 3 colors to colors list
+        colors.add("purple");
+        colors.add("blue");
+        colors.add("yellow");
+        // Colors can only receive strings
+        // colors.add(1); // ERROR
+        // colors.add(Object()); // ERROR
+        // Print the array list
+        System.out.println(colors);
 
     }
 }
