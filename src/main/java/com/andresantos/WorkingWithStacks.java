b075ae520b662f8eb11eafb3333d067a20398740
ade-re10 <andreandre_1234@hotmail.com>
2022-11-20 11:07:21 +0100

    Print the top item, the size, remove the top item and if the stack is empty or not

--- src/main/java/com/andresantos/WorkingWithStacks.java
+++ src/main/java/com/andresantos/WorkingWithStacks.java
@@ -11,6 +11,16 @@ public class WorkingWithStacks {
         stack.push(1);
         stack.push(2);
         stack.push(3);
+        // Print the top item of the stack
+        System.out.println(stack.peek());
+        // Print the size of the stack
+        System.out.println(stack.size());
+        // Remove the top item of the stack
+        System.out.println(stack.pop());
+        // Print the new size of the stack
+        System.out.println(stack.size());
+        // Print if the stack is empty with a true of false
+        System.out.println(stack.empty());
 
     }
 }
