2a3914c217eb4df5f45e9a6610b44ef4c01dff9a
ade-re10 <andreandre_1234@hotmail.com>
2022-11-20 11:05:34 +0100

    Add 3 items to the stack

--- src/main/java/com/andresantos/WorkingWithStacks.java
+++ src/main/java/com/andresantos/WorkingWithStacks.java
@@ -7,6 +7,10 @@ public class WorkingWithStacks {
     public static void main (String[] args) {
         // Create a new Stack of Integers
         Stack<Integer> stack = new Stack<>();
+        // Add 3 numbers to the stack
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
 
     }
 }
