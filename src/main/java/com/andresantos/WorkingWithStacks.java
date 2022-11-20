18da8d20c7a5f7cb26aa07c75566e18602d92448
ade-re10 <andreandre_1234@hotmail.com>
2022-11-20 11:05:03 +0100

    Create a new Stack of integers

---
+++ src/main/java/com/andresantos/WorkingWithStacks.java
@@ -0,0 +1,12 @@
+package com.andresantos;
+
+import java.util.List;
+import java.util.Stack;
+
+public class WorkingWithStacks {
+    public static void main (String[] args) {
+        // Create a new Stack of Integers
+        Stack<Integer> stack = new Stack<>();
+
+    }
+}
