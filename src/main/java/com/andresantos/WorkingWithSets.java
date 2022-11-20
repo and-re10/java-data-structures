eb2a8e31f7ec2dad1a09fe9b239b337e84e0493b
ade-re10 <andreandre_1234@hotmail.com>
2022-11-20 13:23:34 +0100

    Add the override for equals, hashCode and String to not repeat items in the Ball Set

--- src/main/java/com/andresantos/WorkingWithSets.java
+++ src/main/java/com/andresantos/WorkingWithSets.java
@@ -37,6 +37,25 @@ public class WorkingWithSets {
             this.color = color;
         }
 
+        // Add the override for equals, hashCode and String to not repeat items in the Ball Set
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Ball ball = (Ball) o;
+            return Objects.equals(color, ball.color);
+        }
 
+        @Override
+        public int hashCode() {
+            return Objects.hash(color);
+        }
+
+        @Override
+        public String toString() {
+            return "Ball{" +
+                    "color='" + color + '\'' +
+                    '}';
+        }
     }
 }
