117c4b82842202a759767194cafe84e7db2124b7
ade-re10 <andreandre_1234@hotmail.com>
2022-11-20 15:43:51 +0100

    Add 3 persons to the map

--- src/main/java/com/andresantos/WorkingWithMaps.java
+++ src/main/java/com/andresantos/WorkingWithMaps.java
@@ -7,6 +7,10 @@ public class WorkingWithMaps {
     public static void main (String[] args) {
         // Create a new Map with persons
         Map<Integer, Person> map = new HashMap<>();
+        // Add 3 persons to the map
+        map.put(1, new Person("Alex"));
+        map.put(2, new Person("Alexa"));
+        map.put(3, new Person("Ali"));
 
     }
 
