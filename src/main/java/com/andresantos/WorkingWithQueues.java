27efa1c94c59899588113041de4cc4883d685b97
ade-re10 <andreandre_1234@hotmail.com>
2022-11-20 11:35:17 +0100

    Adding 3 persons to the Queue

--- src/main/java/com/andresantos/WorkingWithQueues.java
+++ src/main/java/com/andresantos/WorkingWithQueues.java
@@ -7,6 +7,10 @@ public class WorkingWithQueues {
     public static void main (String[] args) {
         // Create a new Queue
         Queue<Person> queue = new LinkedList<>();
+        // Add 3 persons to the Queue
+        queue.add(new Person("Alex", 21));
+        queue.add(new Person("Mariam", 18));
+        queue.add(new Person("ALi", 40));
 
     }
 
