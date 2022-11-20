aa901e4478a107fd7b19cdfe1905112493ff2e2f
ade-re10 <andreandre_1234@hotmail.com>
2022-11-20 15:55:16 +0100

    Print size, person with key 1, if contains key 4, all the keys and all the entries of the Map

--- src/main/java/com/andresantos/WorkingWithMaps.java
+++ src/main/java/com/andresantos/WorkingWithMaps.java
@@ -13,8 +13,18 @@ public class WorkingWithMaps {
         map.put(3, new Person("Ali"));
         // Print the map
         System.out.println(map);
-        // Add a new person with an existing key
+        // Add a new person with an existing key - it will override the key
         map.put(3, new Person("Ali 2"));
+        // Print the size of the map
+        System.out.println(map.size());
+        // Print the person with the key 1
+        System.out.println(map.get(1));
+        // Print if the map contains the key 4 (true or false)
+        System.out.println(map.containsKey(4));
+        // Print all the existing keys
+        System.out.println(map.keySet());
+        // Print all the entries
+        System.out.println(map.entrySet());
     }
 
     static record Person (String name) {}
